b6fc4133e92cded21cb9005f9de34ca5ca37c486
aimunNaharr <aimunnaharr@gmail.com>
2023-01-03 18:25:31 +0600

    project initialized

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Banner from './components/banner'
+
+export default function Home() {
+  return (
+    <div className=''>
+    <Banner/>
+    </div>
+  )
+}
