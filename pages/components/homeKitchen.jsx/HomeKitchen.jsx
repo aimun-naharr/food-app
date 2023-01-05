1e728791baeec5fb90e608c88d1f3235babcfe8f
aimunNaharr <aimunnaharr@gmail.com>
2023-01-05 10:15:33 +0600

    updated

--- pages/components/homeKitchen.jsx/HomeKitchen.jsx
+++ pages/components/homeKitchen.jsx/HomeKitchen.jsx
@@ -14,7 +14,7 @@ const HomeKitchen = () => {
                     <div className="grid  w-full  justify-center items-center my-[80px]">
                         <div className="grid grid-cols-4 justify-between items-baseline gap-8">
                         {
-                              data?.map(product=><Product key={product.id} product={product}/>)
+                              data?.slice(0, 4).map(product=><Product key={product.id} product={product}/>)
                          }
                         </div>
                          <button className="border-2 w-[15%] py-2 rounded-md bg-inherit text-gray-400 mt-[80px] mb-[80px] mx-auto border-[#F3BA00] ">+   Load more...</button>
